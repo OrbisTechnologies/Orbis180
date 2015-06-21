9e8f5407a48df40e5a8b2d82f9aee8e816eaf727
Ankit Parmar <aparmar@orbistechnologies.com>
2015-06-21 08:21:54 -0700

    Ticket#1 Created a REST service class that query openFDA database and pull the data from their database

---
+++ dataIngestion/src/main/java/com/orbis/orbis180/rest/ProjectStatus.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.orbis.orbis180.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+/**
+ *
+ * @author aparmar
+ */
+@Path("/status")
+public class ProjectStatus {
+
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  public String sayHtmlStatus() {
+    return "<html> " + "<title>" + "Webservice Status" + "</title>"
+        + "<body><h1>" + "Orbis180 Data Ingestion Webservice is Alive" + "</body></h1>" + "</html> ";  
+  }
+  
+  @GET()
+  @Path("/getOpenFDAData")
+  @Produces(MediaType.TEXT_HTML)  
+  public void test(){
+  GetOpenFDAData test = new GetOpenFDAData();
+  test.getOpenFDAData();
+  }
+      
+}
