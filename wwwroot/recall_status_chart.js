09fe4976a44af3cb01eaa98cffa2dcfde525eb70
unknown <aarias@AARIAS-LT01.orbis.private>
2015-06-24 15:57:24 -0400

    #15 #18 #19 #20 #21 #22 #27 #28 #29 #30 #31 #32 #35 #64 #65 #66 #67 #68 #69 #70Satisfies the UI content for the overview page including graphs and statistics

--- wwwroot/recall_status_chart.js
+++ wwwroot/recall_status_chart.js
@@ -1,193 +1,41 @@
-var svg = d3.select("recall_chart")
-	.append("svg")
-	.append("g")
+/*
+ *A C3 widget that displays overall statistical data for total recalls in the form of 
+ *a donut pie chart. Data that is displayed is the percentage of recalls based on the
+ *categories of: Ongoing, Completed, Terminated.
+ * @author Apeenan Arias
+ */
 
-svg.append("g")
-	.attr("class", "slices");
-svg.append("g")
-	.attr("class", "labels");
-svg.append("g")
-	.attr("class", "lines");
-
-
-var pie = d3.layout.pie()
-	.sort(null)
-	.value(function(d) {
-		return d.value;
-	});
-
-var arc = d3.svg.arc()
-	.outerRadius(radius * 0.8)
-	.innerRadius(radius * 0.4);
-
-var outerArc = d3.svg.arc()
-	.innerRadius(radius * 0.9)
-	.outerRadius(radius * 0.9);
-
-svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-var key = function(d){ return d.data.label; };
-
-var color = d3.scale.ordinal()
-	.domain(["Ongoing", "Completed","Terminated"])
-	.range(["#98abc5","#ff8c00","#a05d56"]);
-
-function combineData(json1,json2,json3){
-	var arr = [];
-	arr.push(json1.meta.results.total);
-	arr.push(json2.meta.results.total);
-	arr.push(json3.meta.results.total);
-	return arr;
-		
-}
-function randomData (totalcount){
-	var labels = color.domain();
-	return labels.map(function(label){
-		var count = 0;
-		if(label == "Ongoing"){count = totalcount[0];}
-		else if(label == "Completed"){count = totalcount[1];}
-		else if(label == "Terminated"){count = totalcount[2];}
-		return { label: label, value:  count}
-	});
-}
-
-function change(data) {
-
-	/* ------- PIE SLICES -------*/
-	var slice = svg.select(".slices").selectAll("path.slice")
-		.data(pie(data), key);
-
-	slice.enter()
-		.insert("path")
-		.style("fill", function(d) { return color(d.data.label); })
-		.attr("class", "slice");
-
-	slice		
-		.transition().duration(1000)
-		.attrTween("d", function(d) {
-			this._current = this._current || d;
-			var interpolate = d3.interpolate(this._current, d);
-			this._current = interpolate(150);
-			return function(t) {
-				return arc(interpolate(t));
-			};
-		})
-
-	slice.exit()
-		.remove();
-
-	/* ------- TEXT LABELS -------*/
-
-	var text = svg.select(".labels").selectAll("text")
-		.data(pie(data), key);
-
-	text.enter()
-		.append("text")
-		.attr("dy", ".40em")
-		.text(function(d) {
-			return d.data.label;
-		});
-	
-	function midAngle(d){
-		return d.startAngle + (d.endAngle - d.startAngle)/4;
-	}
-
-	text.transition().duration(1000)
-		.attrTween("transform", function(d) {
-			this._current = this._current || d;
-			var interpolate = d3.interpolate(this._current, d);
-			this._current = interpolate(90);
-			return function(t) {
-				var d2 = interpolate(t);
-				var pos = outerArc.centroid(d2);
-				pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
-				return "translate("+ pos +")";
-			};
-		})
-		.styleTween("text-anchor", function(d){
-			this._current = this._current || d;
-			var interpolate = d3.interpolate(this._current, d);
-			this._current = interpolate(90);
-			return function(t) {
-				var d2 = interpolate(t);
-				return midAngle(d2) < Math.PI ? "start":"end";
-			};
-		});
-
-	text.exit()
-		.remove();
-
-	/* ------- SLICE TO TEXT POLYLINES -------*/
-
-	var polyline = svg.select(".lines").selectAll("polyline")
-		.data(pie(data), key);
-	
-	polyline.enter()
-		.append("polyline");
-
-	polyline.transition().duration(1000)
-		.attrTween("points", function(d){
-			this._current = this._current || d;
-			var interpolate = d3.interpolate(this._current, d);
-			this._current = interpolate(0);
-			return function(t) {
-				var d2 = interpolate(t);
-				var pos = outerArc.centroid(d2);
-				pos[0] = radius * 0.80 * (midAngle(d2) < Math.PI ? 1 : -1);
-				return [arc.centroid(d2), outerArc.centroid(d2), pos];
-			};			
-		});
-	
-	polyline.exit()
-		.remove();
-};
-
-var class_I = (function(){
-			  var class_I = null;
+var recall_status = (function(){
+			  var recall_status = null;
 			  $.ajax({
 			  'async': false,
-	          url: 'https://api.fda.gov/food/enforcement.json?search=status:"Ongoing"',
-			  success: function(){console.log("ajax success bubble");},
-			  error: function(XMLHttpRequest, textStatus, errorThrown) { 
-                    console.log("Status: " + textStatus); console.log("Error: " + errorThrown); 
-                }}
+	          url: 'https://api.fda.gov/food/enforcement.json?count=status.exact',
+			  }
 				).done(function(data) {
-				class_I = data; 
+				recall_status = data; 
   });
-  return class_I;
-  })();  
+  return recall_status;
+  })(); 
   
-var class_II = (function(){
-			  var class_II = null;
-			  $.ajax({
-			  'async': false,
-	          url: 'https://api.fda.gov/food/enforcement.json?search=status:"Completed"',
-			  success: function(){console.log("ajax success bubble");},
-			  error: function(XMLHttpRequest, textStatus, errorThrown) { 
-                    console.log("Status: " + textStatus); console.log("Error: " + errorThrown); 
-                }}
-				).done(function(data) {
-				class_II = data; 
-  });
-  return class_II;
-  })();
-  
-  var class_III = (function(){
-			  var class_III = null;
-			  $.ajax({
-			  'async': false,
-	          url: 'https://api.fda.gov/food/enforcement.json?search=status:"Terminated"',
-			  success: function(){console.log("ajax success bubble");},
-			  error: function(XMLHttpRequest, textStatus, errorThrown) { 
-                    console.log("Status: " + textStatus); console.log("Error: " + errorThrown); 
-                }}
-				).done(function(data) {
-				class_III = data; 
-  });
-  return class_III;
-  })();
-  var fullArray = combineData(class_I,class_II,class_III);
-  console.log("Total number of results for class I " + class_I.meta.results.total);
-  console.log("Total number of results for class II " + class_II.meta.results.total);
-  console.log("Total number of results for class III " + class_III.meta.results.total);
-  change(randomData(fullArray));
+var chart = c3.generate({
+	bindto: '#recall_status',
+	size: {
+  width: 300,
+  height: 300
+},
+    data: {
+        columns: [
+            [recall_status.results[0].term, recall_status.results[0].count],
+			[recall_status.results[1].term, recall_status.results[1].count],
+			[recall_status.results[2].term, recall_status.results[2].count]
+            
+        ],
+        type : 'donut',
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+    },
+    donut: {
+        title: "Recall Status"
+    }
+});
---
+++ wwwroot/timeseries.js
@@ -0,0 +1,75 @@
+//https://api.fda.gov/food/enforcement.json?search=recall_initiation_date:[20150101+TO+20151231]&count=recall_initiation_date
+//
+
+/*
+ *Make an ajax request to return the recall dates across the year from January 2015 to December 2015
+ */
+var food_json = (function(){
+			  var food_json = null;
+			  $.ajax({
+			  'async': false,
+	          url: 'https://api.fda.gov/food/enforcement.json?api_key=LEsaCnFqu5NnXynoyvcNjtaSTEde5r7inxEfcTIH&search=recall_initiation_date:[20150101+TO+20150622]&count=recall_initiation_date' }
+				).done(function(data) {
+				food_json = data; 
+  });
+  return food_json;
+  })(); 
+  
+/*
+ *Function getDates
+ *@param json
+ *Function that cycles through the json of recall_dates and formulates an
+ *array comprised of the dates from January 2015 up to December 2015
+ */
+function getDates(json)
+{
+	var dates = [];
+	dates.push('x');
+	for(var i = 1; i < 85; i++)
+		dates.push(json.results[i].time);
+	return dates;
+}
+/*
+ *Function getOccurrence
+ *@param json
+ *Function that cycles through the json of recall dates and formulates an
+ *array comprised of the number of recalls for that specified date
+ */
+function getOccurrence(json)
+{
+	var data = [];
+	data.push('Voluntary Recalls');
+	for(var i=1; i<85;i++)
+		data.push(json.results[i].count);
+	return data;
+}
+//Variable that houses the recall dates to form the x-axis of the timeline graph
+var recall_dates = getDates(food_json).slice();
+
+//Variable that houses the count by recall date that will form the y-axis of the timeline graph
+var recall_occurrence = getOccurrence(food_json).slice();
+var chart = c3.generate({
+    bindto: '#timeseries',
+	size: {
+  width: 1100,
+},
+    data: {
+        x: 'x',
+        xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
+        columns: [
+			recall_dates,
+			recall_occurrence
+        ]
+    },
+	subchart: {
+        show: true
+    },
+    axis: {
+        x: {
+            type: 'timeseries',
+            tick: {
+                format: '%Y-%m-%d'
+            }
+        }
+    }
+});
